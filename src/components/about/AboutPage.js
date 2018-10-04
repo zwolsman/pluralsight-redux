f343338a181317d657be34805e5fdf58b1c53e59
Marvin <marvin.zwolsman@gmail.com>
2018-10-04 10:14:55 +0200

    About page

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          This application uses React, Redux, React router and a variety of
+          other usefull libraries.
+        </p>
+      </div>
+    );
+  }
+}
