6555288731949e791adf0b36e464ba4c739b05b7
Marvin <marvin.zwolsman@gmail.com>
2018-10-07 19:22:46 +0200

    CoursesPage save and onTitleChange

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 
 export default class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: "" }
+    };
+  }
+
+  onTitleChange = event => {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course });
+  };
+
+  onClickSave() {
+    console.log("Save");
+  }
   render() {
     return (
       <div>
         <h1>Courses Page</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+
+        <input type="submit" value="Save" onClick={this.onClickSave} />
       </div>
     );
   }
