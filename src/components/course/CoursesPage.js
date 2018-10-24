7600f07230dcaa311fec6a1abd927b5aa2b2dc77
Marvin <marvin.zwolsman@gmail.com>
2018-10-24 10:34:37 +0200

    Removed inline form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -5,46 +5,12 @@ import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      course: { title: "" }
-    };
-  }
-
-  onTitleChange = event => {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course });
-  };
-
-  onClickSave = () => {
-    this.props.createCourse(this.state.course);
-  };
-
   render() {
     const { courses } = this.props;
     return (
       <div>
         <h1>Courses Page</h1>
         <CourseList courses={courses} />
-        <h2>Add Course</h2>
-
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            onChange={this.onTitleChange}
-            value={this.state.course.title}
-          />
-        </div>
-        <input
-          className="btn btn-primary"
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
       </div>
     );
   }
