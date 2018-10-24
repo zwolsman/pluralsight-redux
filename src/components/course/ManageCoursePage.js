d460cf842ca829e4b097a8f43a41adb9383562f6
Marvin <marvin.zwolsman@gmail.com>
2018-10-24 11:05:26 +0200

    Added prop-types and using it in the components

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,20 +2,38 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as courseActions from "../../store/actions/courseActions";
-
+import CourseForm from "./CourseForm";
 class ManageCoursePage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
+  }
   render() {
     return (
-      <div>
-        <h1>Manage course</h1>
-      </div>
+      <CourseForm
+        course={this.state.course}
+        errors={this.state.errors}
+        allAuthors={[]}
+      />
     );
   }
 }
 
 function mapStateToProps(state, ownProps) {
+  const course = {
+    id: "",
+    watchHref: "",
+    title: "",
+    authorId: "",
+    length: "",
+    category: ""
+  };
+
   return {
-    state
+    course
   };
 }
 function mapDispatchToProps(dispatch) {
